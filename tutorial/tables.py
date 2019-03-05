b4566e32dce3d5c52037f7c9d022e71ce95fdc93
Paolo Cozzi <bunop@libero.it>
2019-03-05 12:32:38 +0100

    Rendering table with django_tables2

---
+++ tutorial/tables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 12:26:07 2019
+
+@author: Paolo Cozzi <cozzi@ibba.cnr.it>
+"""
+
+import django_tables2 as tables
+from .models import Person
+
+
+# While simple, passing a QuerySet directly to {% render_table %} does not
+# allow for any customization. For that, you must define a custom Table class:
+class PersonTable(tables.Table):
+    class Meta:
+        model = Person
+        template_name = 'django_tables2/bootstrap.html'
--- tutorial/views.py
+++ tutorial/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django_tables2 import RequestConfig
 from .models import Person
+from .tables import PersonTable
+
 
 def people(request):
-    return render(request, 'tutorial/people.html', {'people': Person.objects.all()})
+    table = PersonTable(Person.objects.all())
+
+    # Using RequestConfig automatically pulls values from request.GET and
+    # updates the table accordingly. This enables data ordering and pagination.
+    RequestConfig(request).configure(table)
+    return render(request, 'tutorial/people.html', {'table': table})
